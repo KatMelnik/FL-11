0de045e6ba5a88a3212a16b53bd6f6f2b76393b1
Kateryna Melnyk <51606253+KatMelnik@users.noreply.github.com>
2019-07-07 17:59:12 +0300

    commit task1

---
+++ FL11_HW6/homework/js/task1.js
@@ -0,0 +1,15 @@
+let a1, a2, b1, b2, c1, c2;
+a1 = +prompt('Enter coordinate X for point A', '');
+a2 = +prompt('Enter coordinate Y for point A', '');
+b1 = +prompt('Enter coordinate X for point B', '');
+b2 = +prompt('Enter coordinate Y for point B', '');
+c1 = +prompt('Enter coordinate X for point C', '');
+c2 = +prompt('Enter coordinate Y for point C', '');
+let divHalf = 2;
+if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
+	console.log('Error. Please, enter only number.'); 
+} else if ((a1 + b1)/divHalf===c1 && (a2 + b2)/divHalf===c2) {
+	console.log(true); 
+} else {
+	console.log(false); 
+}
