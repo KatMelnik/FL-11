51b38d95eab6d65896b0edc1f7fb30501f03de0e
Kateryna Melnyk <51606253+KatMelnik@users.noreply.github.com>
2019-07-09 23:44:31 +0300

    commit task2

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,1 +1,39 @@
-// Your code goes here
+
+let minNumber=0;
+let maxNumber=8;
+let intNumber;
+let countTry=3;
+let number;
+let prizeOne=100;
+let prizeTwo=50;
+let prizeThree=25;
+let prize=0;
+let leftTry=countTry;
+let tryOne=0;
+let tryTwo=1;
+let tryThree=2;
+let win, againGame;
+
+if(!confirm('Do you want to play a game?')) {
+	alert('You did not become a billionaire, but can.','');
+} else {
+	intNumber=Math.floor(Math.random()*(maxNumber+1));
+	console.log(intNumber);
+	for(let i=0;i<countTry;i++){
+		number=+prompt('Enter a number (from 0 to 8) of pocket on which the ball could land.','');
+		leftTry=leftTry-1;
+		if (number===intNumber){
+			if (i === tryOne) {
+				prize += prizeOne;
+			} else if (i === tryTwo) {
+				prize += prizeTwo;
+			} else if (i === tryThree) {
+				prize += prizeThree;
+			}
+			win=confirm('Congratulation, you won! Your prize is: ' + prize + '$. \nDo you want to continue?');
+			break;
+		}
+	}
+	alert('Thank you for your participation. Your prize is: '+prize+'$ ');
+	againGame = confirm('Do you want to play again?');   
+}
