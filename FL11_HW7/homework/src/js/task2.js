329883254f47e4e2275e233969123865a1b979eb
Kateryna Melnyk <51606253+KatMelnik@users.noreply.github.com>
2019-07-10 03:20:23 +0300

    add

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,43 +1,57 @@
 
+let prizeOne=100;
+let prizeTwo=50;
+let prizeThree=25;
 let minNumber=0;
 let maxNumber=8;
 let intNumber;
 let countTry=3;
 let number;
-let prizeOne=100;
-let prizeTwo=50;
-let prizeThree=25;
+let possiblePrize=[prizeOne,prizeTwo,prizeThree];
 let prize=0;
 let leftTry=countTry;
 let tryOne=0;
 let tryTwo=1;
 let tryThree=2;
 let win, againGame;
-let text='Enter a number (from 0 to 8) of pocket on which the ball could land.';
+let flag=false;
+let question=confirm('Do you want to play a game?');
+let rangeBig=4;
+let prizeBig=2;
 
-if(!confirm('Do you want to play a game?')) {
+if(!question) {
 	alert('You did not become a billionaire, but can.','');
 } else {
-	intNumber=Math.floor(Math.random()*(maxNumber+1));
-	console.log(intNumber);
-	for(let i=0;i<countTry;i++){
-		number=+prompt('Choose a roulette pocket number from 0 to 8\n'+
-			' \nAttempts left:'+leftTry+
-			' \nTotal prize:'+prize+'$'+
-			'\nPossible prize on current attempt: 100$','');
-		leftTry=leftTry-1;
-		if (number===intNumber){
-			if (i === tryOne) {
-				prize += prizeOne;
-			} else if (i === tryTwo) {
-				prize += prizeTwo;
-			} else if (i === tryThree) {
-				prize += prizeThree;
+	do{
+		leftTry=countTry;
+		intNumber=Math.floor(Math.random()*(maxNumber+1));
+		console.log(intNumber);
+		for(let i=0;i<countTry;i++){
+			number=+prompt('Choose a roulette pocket number from 0 to '+maxNumber+
+				'\nAttempts left:'+leftTry+
+				'\nTotal prize:'+prize+'$'+
+				'\nPossible prize on current attempt:'+possiblePrize[i]+'$','');
+			leftTry=leftTry-1;
+			if (number===intNumber){
+				prize += possiblePrize[i];
+				alert('Congratulation, you won! Your prize is: ' + prize + '$.');
+				flag=true;
+				break;
 			}
-			win=confirm('Congratulation, you won! Your prize is: ' + prize + '$. \nDo you want to continue?');
-			break;
 		}
-	}
-	alert('Thank you for your participation. Your prize is: '+prize+'$ ');
-	againGame = confirm('Do you want to play again?');   
+		if (flag===false) {
+			alert('Thank you for your participation. Your prize is: '+prize+'$ ');
+		}
+		againGame = confirm('Do you want to play again?'); 
+		if (!againGame) {
+			alert('You did not become a billionaire, but can.','');
+		} else if (flag===true){
+			for (let j=0; j<countTry; j++) {
+				possiblePrize[j]=possiblePrize[j]*prizeBig; 
+			} 
+			maxNumber+=rangeBig; 
+			flag=false;
+		}
+		
+	} while (againGame);
 }
