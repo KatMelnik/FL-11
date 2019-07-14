51c20f4832b5d3dab2a7c557d31e1741be97a914
Kateryna Melnyk <51606253+KatMelnik@users.noreply.github.com>
2019-07-14 23:31:45 +0300

    last commit

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -5,6 +5,6 @@ function formatTime(t) {
 	minutes = t - days * 1440 - hours * 60;
 	return days + 'day(s) ' + hours + 'hour(s) ' + minutes + 'minute(s)'; 
 }
-console.log(formatTime(120));
-console.log(formatTime(59));
-console.log(formatTime(3601));
+console.log('formatTime(120);//=>',formatTime(120));
+console.log('formatTime(59);//=>',formatTime(59));
+console.log('formatTime(3601);//=>',formatTime(3601));
