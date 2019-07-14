2d51ffec019fc50ddad65e78611a3fd1f66c9822
Kateryna Melnyk <51606253+KatMelnik@users.noreply.github.com>
2019-07-14 23:17:56 +0300

    add task7

---
+++ FL11_HW8/homework/js/formatTime.js
@@ -0,0 +1,10 @@
+function formatTime(t) {
+	let days, hours, minutes;
+	days = ~~(t / 1440);
+	hours = ~~((t - days * 1440) / 60);
+	minutes = t - days * 1440 - hours * 60;
+	return days + 'day(s) ' + hours + 'hour(s) ' + minutes + 'minute(s)'; 
+}
+console.log(formatTime(120));
+console.log(formatTime(59));
+console.log(formatTime(3601));
